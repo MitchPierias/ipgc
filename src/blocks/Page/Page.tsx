d2216b440623fbed4f6d709d4fc7df14ac9ad7fb
Mitch Pierias <mitch@pierias.com>
2025-07-24 00:59:08 +1000

    fix: types

--- src/blocks/Page/Page.tsx
+++ src/blocks/Page/Page.tsx
@@ -1,6 +1,12 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
-export const Page = ({ blok }) => (
+type PageBlok = {
+  _uid: string;
+  component: "Page";
+  body: any[];
+};
+
+export const Page = ({ blok }: { blok: PageBlok }) => (
   <main {...storyblokEditable(blok)}>
     {blok.body.map((nestedBlok) => (
       <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
