9a0082f92404e5a4165b6b9fa4b9e9e7d3446552
Mitch Pierias <mitch@pierias.com>
2025-12-08 00:08:41 +1000

    feat: footer changes

--- src/blocks/GridLayout/GridLayout.tsx
+++ src/blocks/GridLayout/GridLayout.tsx
@@ -4,19 +4,32 @@ import clsx from "classnames";
 
 interface GridLayoutProps extends Common.ComponentProps {
   layout?: Common.Layout;
+  height?: Common.Height | "auto";
   spacing?: "sm" | "md" | "lg";
+  columns?: 2 | 3 | 4;
+  align?: "top" | "center" | "bottom";
 }
 
 export const GridLayout = ({
   testID,
   layout = "content",
+  height,
+  columns,
+  align,
   ...props
 }: React.PropsWithChildren<GridLayoutProps>) => {
   return (
-    // <SectionContainer testID={testID} width={layout} height={"full"}>
-    <div data-testid={testID} className={clsx(styles.frame, styles[layout])}>
+    <div
+      data-testid={testID}
+      className={clsx(
+        styles.frame,
+        styles[`width-${layout}`],
+        height && styles[`height-${height}`],
+        columns && styles[`columns-${columns}`],
+        align && styles[`align-${align}`]
+      )}
+    >
       {props.children}
     </div>
-    // </SectionContainer>
   );
 };
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,59 +12,58 @@ import {
 import { Title } from "src/elements/Text/Text";
 import { BlockLayout } from "src/elements/BlockLayout/BlockLayout";
 import { Button } from "src/elements/Buttons/Button";
+import { GridLayout } from "src/blocks/GridLayout/GridLayout";
+import { InlineLayout } from "src/elements/InlineLayout/InlineLayout";
 
 interface FooterProps extends Common.ComponentProps {}
 
 export const Footer = ({ testID, ...props }: FooterProps) => {
   return (
     <footer data-testid={testID} className={styles.frame} role="contentinfo">
-      <div className={styles.wrap}>
+      <div
+        data-testid={`${testID}.content`}
+        className={clsx(styles.wrap, styles.contain)}
+      >
         <div className={styles.leftColumn}>
-          <Link href="/">
+          <Link href="/" className={styles.logo}>
             <Image
               src="/images/logo-footer.png"
               alt="Interventional Pain GC"
-              width={420}
-              height={420}
+              width={500}
+              height={122}
             />
           </Link>
 
-          <BlockLayout testID={`${testID}.downloads`}>
-            <Title testID={`${testID}.title`}>Referral Form</Title>
-            <Button
-              testID={`${testID}.download`}
-              href={REFERRAL_FORM_PATH}
-              download={REFERRAL_FORM_DOWNLOAD_NAME}
-            >
-              Download Form
-            </Button>
-          </BlockLayout>
+          {/* <SocialLinks testID="socials" items={socialLinks} /> */}
         </div>
 
-        <div className={styles.rightColumn}>
-          <div className={styles.navigation}>
-            <nav
-              className={styles.navigation}
-              itemScope
-              itemType="https://schema.org/SiteNavigationElement"
-            >
-              <ul className={styles.list}>
-                {navigationItems.map((item) => (
-                  <li key={item.href}>
-                    <Link
-                      data-testid={`${testID}.link`}
-                      href={item.href}
-                      className={styles.link}
-                    >
-                      {item.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
-
-          {/* <SocialLinks testID="socials" items={socialLinks} /> */}
+        <nav
+          className={styles.navigation}
+          itemScope
+          itemType="https://schema.org/SiteNavigationElement"
+        >
+          <ul className={styles.list}>
+            {navigationItems.map((item) => (
+              <li key={item.href}>
+                <Link
+                  data-testid={`${testID}.link`}
+                  href={item.href}
+                  className={styles.link}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className={styles.contain}>
+          <Button
+            testID={`${testID}.download`}
+            href={REFERRAL_FORM_PATH}
+            download={REFERRAL_FORM_DOWNLOAD_NAME}
+          >
+            Download Referral Form
+          </Button>
         </div>
       </div>
     </footer>
